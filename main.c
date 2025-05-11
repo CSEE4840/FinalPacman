0ce5fbf5dfc5a48f0b5fc32577520b454f2154dd
Haoming Ma <mahaoming8585485@163.com>
2025-05-11 18:09:34 -0400

    Update files

--- main.c
+++ main.c
@@ -755,7 +755,7 @@ void game_loop() {
         }
 
         update_all_to_driver();
-        usleep(500000);
+        usleep(100000);
 
         // 如果 Game Over，暂停游戏，直到 reset
         if (*CONTROL_REG & CTRL_GAME_OVER) {
