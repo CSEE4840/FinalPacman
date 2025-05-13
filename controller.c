4ddecaa839c9e071aa23ef5fe091cd1b84e1c5dd
Haoming Ma <mahaoming8585485@163.com>
2025-05-13 15:00:04 -0400

    Update files

--- controller.c
+++ controller.c
@@ -77,9 +77,9 @@ void detect_presses(struct controller_pkt pkt, char *buttons, int mode) {
     if ((pkt.ab & 0x10)) {
         buttons[5] = vals[5]; // X pressed
     }
-    if ((pkt.ab & 0x08)) {
-        buttons[6] = vals[6]; // L pressed
-    }
+//     if ((pkt.ab & 0x08)) {
+//         buttons[6] = vals[6]; // L pressed
+//     }
         // if ((pkt.rl & 0x20)) {
         //         buttons[6] = vals[6]; // L pressed
         // }
